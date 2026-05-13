5d7eed9b0b5baacc0c3d19c4a700c3ac04c179f9
Chris Brenton <hiring@rubyridge.org>
2026-05-13 17:08:00 -0400

    Add interval struct and impl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod helpers;
+mod math;
 
 fn main() {
     dbg!(helpers::random_double());
+    let foo = math::DInterval::EMPTY;
+    dbg!(foo);
 }
---
+++ src/math/interval.rs
@@ -0,0 +1,178 @@
+#![allow(dead_code, unused_imports)]
+
+use std::ops::{Add, Mul, Sub};
+
+pub type DInterval = Interval<f64>;
+pub type IInterval = Interval<i32>;
+
+#[derive(Copy, Clone, Default, Debug, PartialEq)]
+pub struct Interval<T> {
+    pub min: T,
+    pub max: T,
+}
+
+impl<T: Add<Output = T> + Sub<Output = T> + Mul<Output = T> + PartialOrd + Copy> Interval<T> {
+    pub fn new(min: T, max: T) -> Self {
+        Self { min, max }
+    }
+
+    /// Returns the distance between min and max
+    pub fn size(&self) -> T {
+        self.max - self.min
+    }
+
+    /// Whether the range contains the given value (inclusive)
+    pub fn contains(&self, x: T) -> bool {
+        self.min <= x && x <= self.max
+    }
+
+    /// Whether the range surrounds the given value (exclusive)
+    pub fn surrounds(&self, x: T) -> bool {
+        self.min < x && x < self.max
+    }
+
+    /// Clamp the given value to the range's min and max
+    pub fn clamp(&self, x: T) -> T {
+        if x < self.min {
+            self.min
+        } else if x > self.max {
+            self.max
+        } else {
+            x
+        }
+    }
+}
+
+impl DInterval {
+    pub const EMPTY: Self = Interval {
+        min: f64::INFINITY,
+        max: f64::NEG_INFINITY,
+    };
+
+    pub const UNIVERSE: Self = Interval {
+        min: f64::NEG_INFINITY,
+        max: f64::INFINITY,
+    };
+
+    /// Converts range (0.0..1.0) to (min..max)
+    pub fn scale(&self, x: f64) -> f64 {
+        x * self.size() + self.min
+    }
+}
+
+impl IInterval {
+    pub const EMPTY: Self = Interval {
+        min: i32::MAX,
+        max: i32::MIN,
+    };
+
+    pub const UNIVERSE: Self = Interval {
+        min: i32::MIN,
+        max: i32::MAX,
+    };
+
+    /// Converts range (0.0..1.0) to (min..max)
+    pub fn scale(&self, x: f64) -> i32 {
+        (x * self.size() as f64) as i32 + self.min
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_size_returns_max_minus_min() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert_eq!(ival.size(), 2.0);
+    }
+
+    #[test]
+    fn test_contains_val_inside_range_returns_true() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert!(ival.contains(0.0));
+    }
+
+    #[test]
+    fn test_contains_val_outside_range_returns_false() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert!(!ival.contains(2.0));
+    }
+
+    #[test]
+    fn test_contains_val_on_boundary_returns_true() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert!(ival.contains(1.0));
+    }
+
+    #[test]
+    fn test_surrounds_val_inside_range_returns_true() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert!(ival.surrounds(0.0));
+    }
+
+    #[test]
+    fn test_surrounds_val_outside_range_returns_false() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert!(!ival.surrounds(2.0));
+    }
+
+    #[test]
+    fn test_surrounds_val_on_boundary_returns_false() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert!(!ival.surrounds(1.0));
+    }
+    #[test]
+    fn test_clamp_below_min_returns_min() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert_eq!(ival.clamp(-1.1), -1.0);
+    }
+
+    #[test]
+    fn test_clamp_above_max_returns_max() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert_eq!(ival.clamp(1.1), 1.0);
+    }
+
+    #[test]
+    fn test_clamp_in_range_returns_val() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert_eq!(ival.clamp(0.1), 0.1);
+    }
+
+    #[test]
+    fn test_dinterval_scale_converts_0_to_min() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert_eq!(ival.scale(0.0), ival.min);
+    }
+
+    #[test]
+    fn test_dinterval_scale_converts_1_to_max() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert_eq!(ival.scale(1.0), ival.max);
+    }
+
+    #[test]
+    fn test_dinterval_scale_converts_point_5_to_midpoint() {
+        let ival = DInterval::new(-1.0, 1.0);
+        assert_eq!(ival.scale(0.5), ival.min + (ival.max - ival.min) / 2.0);
+    }
+
+    #[test]
+    fn test_iinterval_scale_converts_0_to_min() {
+        let ival = IInterval::new(-1, 1);
+        assert_eq!(ival.scale(0.0), ival.min);
+    }
+
+    #[test]
+    fn test_iinterval_scale_converts_1_to_max() {
+        let ival = IInterval::new(-1, 1);
+        assert_eq!(ival.scale(1.0), ival.max);
+    }
+
+    #[test]
+    fn test_iinterval_scale_converts_point_5_to_midpoint() {
+        let ival = IInterval::new(-1, 1);
+        assert_eq!(ival.scale(0.5), ival.min + (ival.max - ival.min) / 2);
+    }
+}

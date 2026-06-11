d9422922fa3633b2e5027076d7a6c1620fa8ded3
Chris Brenton <hiring@rubyridge.org>
2026-06-10 20:37:48 -0400

    Move Camera into SceneData, which is now its own file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-use std::f64::consts::PI;
-
-use glam::DVec3;
 use kdam::{BarExt, tqdm};
-use rt2::math::Lerp;
-use rt2::math::Rotate;
-use rt2::render::CameraState;
-use rt2::render::CameraTransition;
 use rt2::render::Film;
 use rt2::render::MonteCarloRenderer;
 use rt2::render::MultiFilePngWriter;
-use rt2::render::VideoCamera;
 use rt2::scene::sample_scene;
 
 fn main() {
@@ -23,50 +15,16 @@ fn main() {
         samples_per_pixel: 10,
     };
 
-    let start = CameraState {
-        pos: DVec3::new(0.0, 0.0, 3.0),
-        look_at: DVec3::new(0.0, 0.0, -1.0),
-        up: DVec3::new(0.0, 1.0, 0.0),
-    };
-    let end = CameraState {
-        pos: DVec3::new(0.0, 0.0, 0.5),
-        look_at: DVec3::new(0.0, 0.0, -1.0),
-        up: DVec3::new(0.0, 1.0, 0.0),
-    };
-    let look_at_hold = Lerp::hold(start.look_at);
-    let up_hold = Lerp::hold(start.up);
-
-    let mut camera = VideoCamera::new(&start, 90.0, renderer, film);
-
-    let zoom_in_slerp = Lerp::end_early(start.pos, end.pos, 0.8, true);
-    camera.add_transition(CameraTransition::new(
-        zoom_in_slerp,
-        look_at_hold.clone(),
-        up_hold.clone(),
-        100,
-    ));
-
-    let zoom_out_slerp = Lerp::new(end.pos, start.pos, true);
-    camera.add_transition(CameraTransition::new(
-        zoom_out_slerp,
-        look_at_hold.clone(),
-        up_hold.clone(),
-        100,
-    ));
-
-    let rot = Rotate::end_early(start.pos, start.look_at, 2.0 * PI, 0.5, true);
-    camera.add_transition(CameraTransition::new(rot, look_at_hold, up_hold, 100));
-
     let image_writer = MultiFilePngWriter::new("./output", "frame_{{frame_number}}");
 
-    let scene = sample_scene();
+    let scene = sample_scene(renderer, film);
 
-    let mut progress = tqdm!(total = camera.total_frames);
+    let mut progress = tqdm!(total = scene.camera.total_frames);
     progress.refresh().unwrap();
 
     let mut errs = vec![];
 
-    for frame in camera.render_frames(&scene.world) {
+    for frame in scene.camera.render_frames(&scene.world) {
         if let Err(val) = image_writer.write(&frame) {
             errs.push(format!("frame {}: {}", frame.frame_number, val));
         }
--- src/scene/mod.rs
+++ src/scene/mod.rs
@@ -1,3 +1,5 @@
 mod sample;
+mod scene_data;
 
-pub use sample::*;
+pub use sample::sample_scene;
+pub use scene_data::SceneData;

4e8a0fcce95d564cb1de70841d9ae2162faf909a
Chris Brenton <chrisbrenton@gmail.com>
2026-06-11 22:10:52 +0000

    Fix a bug when writing pixels with components greater than 1 (#15)
    Co-authored-by: Chris Brenton <hiring@rubyridge.org>

--- src/math/color.rs
+++ src/math/color.rs
@@ -5,7 +5,7 @@ pub type Color = DVec3;
 
 pub fn linear_to_gamma(linear_component: f64) -> f64 {
     if linear_component > 0.0 {
-        linear_component.sqrt()
+        linear_component.sqrt().clamp(0.0, 1.0)
     } else {
         0.0
     }
@@ -13,15 +13,22 @@ pub fn linear_to_gamma(linear_component: f64) -> f64 {
 
 #[cfg(test)]
 mod tests {
+    use approx::assert_relative_eq;
+
     use super::*;
 
     #[test]
     fn test_linear_to_gamma_negative_value_returns_zero() {
-        assert_eq!(linear_to_gamma(-1.0), 0.0);
+        assert_relative_eq!(linear_to_gamma(-1.0), 0.0);
     }
 
     #[test]
     fn test_linear_to_gamma_positive_value_returns_sqrt() {
-        assert_eq!(linear_to_gamma(4.0), 2.0);
+        assert_relative_eq!(linear_to_gamma(0.09), 0.3);
+    }
+
+    #[test]
+    fn test_linear_to_gamma_positive_value_clamps_result() {
+        assert_relative_eq!(linear_to_gamma(4.0), 1.0);
     }
 }
